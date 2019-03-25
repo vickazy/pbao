000450b399bf2304dae61af3620882bb8d928048
rendy44 <rendy.de.p@gmail.com>
2019-03-25 10:10:00 +0700

    Change behaviour for localizing script

--- inc/class/class-assets.php
+++ inc/class/class-assets.php
@@ -14,13 +14,6 @@ if ( ! class_exists( 'Class_Assets' ) ) {
 	class Class_Assets {
 
 		/**
-		 * Define localize var
-		 *
-		 * @var array
-		 */
-		private $public_vars = [];
-
-		/**
 		 * Define css files
 		 *
 		 * @var array
@@ -84,28 +77,27 @@ if ( ! class_exists( 'Class_Assets' ) ) {
 		 */
 		private function _map_public_assets() {
 			$this->public_css = [
-				'stylesheet'           => get_stylesheet_uri(),
-				'bootstrap'            => TEMP_URI . '/assets/vendor/bootstrap/css/bootstrap.min.css',
-				'bootstrap-datepicker' => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
-				'font-awesome'         => TEMP_URI . '/assets/vendor/fontawesome-free/css/all.min.css',
-				'open-sans-gf'         => 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800',
-				'merry-weather-gf'     => 'https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic',
-				'main'                 => TEMP_URI . '/assets/landing/css/main.css'
-			];
-
-			$this->public_vars = [
-				[ 'ajax_url' => admin_url( 'admin-ajax.php' ) ]
+				'stylesheet'           => [ 'url' => get_stylesheet_uri() ],
+				'bootstrap'            => [ 'url' => TEMP_URI . '/assets/vendor/bootstrap/css/bootstrap.min.css' ],
+				'bootstrap-datepicker' => [ 'url' => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css' ],
+				'font-awesome'         => [ 'url' => TEMP_URI . '/assets/vendor/fontawesome-free/css/all.min.css' ],
+				'open-sans-gf'         => [ 'url' => 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' ],
+				'merry-weather-gf'     => [ 'url' => 'https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' ],
+				'main'                 => [ 'url' => TEMP_URI . '/assets/landing/css/main.css' ],
 			];
 
 			$this->public_js = [
-				'bootstrap'                  => TEMP_URI . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
-				'bootstrap-datepicker'       => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
-				'bootstrap-datepicker-local' => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.id.min.js',
-				'jquery-easing'              => TEMP_URI . '/assets/vendor/jquery-easing/jquery.easing.min.js',
-				'jquery-validation'          => TEMP_URI . '/assets/vendor/jquery-validation/dist/jquery.validate.min.js',
-				'jquery-validation-local'    => TEMP_URI . '/assets/vendor/jquery-validation/dist/localization/messages_id.min.js',
-				'scroll-reveal'              => TEMP_URI . '/assets/vendor/scrollreveal/scrollreveal.min.js',
-				'main'                       => TEMP_URI . '/assets/landing/js/main.js'
+				'bootstrap'                  => [ 'url' => TEMP_URI . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js' ],
+				'bootstrap-datepicker'       => [ 'url' => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js' ],
+				'bootstrap-datepicker-local' => [ 'url' => TEMP_URI . '/assets/vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.id.min.js' ],
+				'jquery-easing'              => [ 'url' => TEMP_URI . '/assets/vendor/jquery-easing/jquery.easing.min.js' ],
+				'jquery-validation'          => [ 'url' => TEMP_URI . '/assets/vendor/jquery-validation/dist/jquery.validate.min.js' ],
+				'jquery-validation-local'    => [ 'url' => TEMP_URI . '/assets/vendor/jquery-validation/dist/localization/messages_id.min.js' ],
+				'scroll-reveal'              => [ 'url' => TEMP_URI . '/assets/vendor/scrollreveal/scrollreveal.min.js' ],
+				'main'                       => [
+					'url'  => TEMP_URI . '/assets/landing/js/main.js',
+					'vars' => [ 'ajax_url' => admin_url( 'admin-ajax.php' ) ]
+				],
 			];
 		}
 
@@ -120,19 +112,16 @@ if ( ! class_exists( 'Class_Assets' ) ) {
 		 * Callback for loading public assets
 		 */
 		function public_assets_callback() {
-			foreach ( $this->public_js as $name => $url ) {
-				wp_enqueue_script( $name, $url, array( 'jquery' ), '', true );
+			foreach ( $this->public_js as $name => $obj ) {
+				wp_enqueue_script( $name, $obj['url'], array( 'jquery' ), '', true );
+				if ( ! empty( $obj['vars'] ) ) {
+					wp_localize_script( $name, 'obj', $obj['vars'] );
+				}
 			}
 
-			foreach ( $this->public_vars as $var ) {
-				wp_localize_script( 'main', 'obj', $var );
+			foreach ( $this->public_css as $name => $obj ) {
+				wp_enqueue_style( $name, $obj['url'] );
 			}
-
-			foreach ( $this->public_css as $name => $url ) {
-				wp_enqueue_style( $name, $url );
-			}
-
-
 		}
 
 		/**
