102b8d7a788cbf60f26723c0335c42635713a029
rendy44 <rendy.de.p@gmail.com>
2019-04-05 20:48:12 +0700

    Change landing page title

--- header.php
+++ header.php
@@ -14,5 +14,5 @@ do_action( 'header_content' );
 /** If current page is not app page it will be treated as landing page, which needs top nav bar */
 if ( ! is_app() ) {
 	global $temp;
-	echo $temp->render( 'landing-top-nav' );
+	echo $temp->render( 'landing-top-nav', [ 'title' => PREFIX ] );
 }
